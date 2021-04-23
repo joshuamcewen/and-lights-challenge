8c9f3ee2d8b0a8ad40fefe8549476282632c292d
Josh McEwen <josh.mcewen@and.digital>
2021-04-23 12:34:10 +0100

    Add test for final test data

--- src/index.test.js
+++ src/index.test.js
@@ -10,4 +10,22 @@ describe("calculate", () => {
 
     expect(calculate(inputs)).toEqual(9);
   });
+
+  it("Returns 34 for given inputs", () => {
+    const inputs = [
+      [36, 40],
+      [8, 9],
+      [19, 21],
+      [6, 23],
+      [1, 5],
+      [10, 12],
+      [14, 18],
+      [27, 31],
+      [12, 16],
+      [33, 34],
+      [41, 45],
+    ];
+
+    expect(calculate(inputs)).toEqual(34);
+  });
 });
